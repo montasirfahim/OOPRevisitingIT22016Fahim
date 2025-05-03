3b7a2b3baac3e2a4f77383b9d35e48679a27f3b7
Fahim Montasir <montasirtuhin@gmail.com>
2025-05-03 10:25:03 +0600

    Added some exercises

--- src/MultipleInheritance.java
+++ src/MultipleInheritance.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class MultipleInheritance {
+interface AnimalEat {
+    void eat();
+}
+interface AnimalTravel {
+    void travel();
+}
+class Animal implements AnimalEat, AnimalTravel {
+    public void eat() {
+        System.out.println("Animal is eating");
+    }
+    public void travel() {
+        System.out.println("Animal is travelling");
+    }
+}
+public class MultipleInheritance {
+    public static void main(String[] args) {
+        Animal a = new Animal();
+        a.eat();
+        a.travel();
+    }
 }
